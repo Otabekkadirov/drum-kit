ef3ed377a6b75024835098b9ad69622f81cc4a0f
Otabekkadirov <ganijanovich94@gmail.com>
2023-11-10 15:22:27 +0500

    feat: complete drum kit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.addEventListener("keydown", playing);
 
 function playing(event) {
-    const key = document.querySelector("." + event.key);
-
+    const key = document.querySelector("div." + event.key);
+    const audio = document.querySelector("audio." + event.key);
     if (key == null) return;
-
+    audio.currentTime = 0;
+    audio.play();
     key.classList.add("playing");
-
     setTimeout(function () {
         key.classList.remove("playing");
     }, 200);
